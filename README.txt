5360920843e96e380f07ef3baf695676d3970209
Aliusus <jansonsander@gmail.com>
2026-07-10 11:31:51 +0200

    Add files via upload

---
+++ README.txt
@@ -0,0 +1,15 @@
+Anne&Max Bezoekenkaart – installatie
+
+Dit is een Progressive Web App (PWA).
+
+Belangrijk:
+Een PWA moet via een beveiligde website (https) of lokale webserver worden geopend.
+Alleen het index.html-bestand openen is niet genoeg om de app te installeren.
+
+Eenvoudige opties:
+1. Upload de map naar Netlify, GitHub Pages, Cloudflare Pages of een andere https-webhost.
+2. Open daarna de website op je telefoon.
+3. Android/Chrome: tik op 'Installeer app' of kies 'Toevoegen aan startscherm'.
+4. iPhone/Safari: Deel-knop > Zet op beginscherm.
+
+De bezochte filialen worden lokaal op het apparaat opgeslagen.
---
+++ sw.js
@@ -0,0 +1,41 @@
+const CACHE_NAME = 'annemax-kaart-v1';
+const APP_SHELL = [
+  './',
+  './index.html',
+  './manifest.webmanifest',
+  './icons/icon-192.png',
+  './icons/icon-512.png',
+  './icons/icon-512-maskable.png',
+  'https://unpkg.com/leaflet@1.9.4/dist/leaflet.css',
+  'https://unpkg.com/leaflet@1.9.4/dist/leaflet.js'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(APP_SHELL))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', event => {
+  const req = event.request;
+  event.respondWith(
+    caches.match(req).then(cached => {
+      if (cached) return cached;
+      return fetch(req).then(response => {
+        const copy = response.clone();
+        caches.open(CACHE_NAME).then(cache => cache.put(req, copy));
+        return response;
+      }).catch(() => caches.match('./index.html'));
+    })
+  );
+});
